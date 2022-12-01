f24980e0120ddc0102287f1fa6a8f5c5026f6658
Evantf1 <efrisbie2232@gmail.com>
2022-11-30 19:28:41 -0500

    few updates

---
+++ components/loginPage.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LoginPage() {
+    return (
+        <View>
+            <Text>
+                This is the Login Page Component. Will house Auth0 authentication flow. 
+            </Text>
+        </View>
+    )
+}
---
+++ components/mainApp.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+export default function mainAppBuild() {
+    return (
+        <View>
+            <Text>
+                This is the Build for the Main Page.
+            </Text>
+        </View>
+    )
+}
