cf8f21317411dd0efc010634a1b0e4af59d8640b
Evantf1 <efrisbie2232@gmail.com>
2022-12-02 23:11:29 -0500

    Profile moved to Component

--- App.js
+++ App.js
@@ -19,29 +19,21 @@ import { initializeAuth } from 'firebase/auth';
 import { getReactNativePersistence } from 'firebase/auth/react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebaseConfig from './firebase.js';
+
+
 /*
 const app = initializeApp(firebaseConfig);
 initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 })
 */
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const windowHeight = Dimensions.get('window').height - 250;
-const windowHeightFull = Dimensions.get('window').height + 5;
-const windowWidth = Dimensions.get('window').width;
-
 export default function App() {
   return (
-    <View>
-      <LoginPage/>
-    </View>
-  );
-};
-
-/*
-   <NavigationContainer>
+    <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen 
           name="Feed"
@@ -54,6 +46,13 @@ export default function App() {
           component={Profile}/>
       </Tab.Navigator>
     </NavigationContainer> 
+  );
+};
+
+/*
+   <View>
+    <LoginPage/>
+   </View>
 */
 
 //Concept for profile button
@@ -79,105 +78,11 @@ const Feed = ({ navigation }) => {
 }
 
 const Profile = ({ navigation }) => {
-  const [loadState, setLoadState] = useState(0)
-  const [data, setData] = useState([])
-    
-  if (loadState == 0) {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = (e) => {
-    if (request.readyState !== 4) {
-        return;
-    }
-    if (request.status === 200) {
-        console.log('success', request.responseText);
-        ResObj = JSON.parse(request.response)
-        setData(ResObj)
-        setLoadState(2)
-    } else {
-        console.warn('error');
-    }
-    };
-    request.open('GET', 'https://userend.herokuapp.com/get_user/636ef181373c83c3a85a2edf');
-    request.send()
-    setLoadState(1)
-    return(
-      <View>
-        <Text>
-          Loading...
-        </Text>
-      </View>
-    )
-  }
-
-  if ( loadState == 2 ) {
-    if ( Platform.OS === 'ios' ) {
-      return (
-      <View 
-      style={{
-        backgroundColor: 'lightgrey',
-        padding: 8,
-        height: windowHeightFull,
-        position: 'absolute',
-        top: -2
-      }}>
-        <View
-          style={{
-            borderBottomWidth: 3,
-            borderBottomColor: 'black',
-            height: 220,
-            left: -8,
-            top: -10,
-            width: windowWidth,
-            backgroundColor: 'white'
-          }}>
-            <Image
-            source={pfp}
-            style={{
-              height: 100,
-              width: 100,
-              borderRadius: 65,
-              borderColor: 'black',
-              borderWidth: 1,
-              position: 'absolute',
-              top: 35,
-              left: 10
-            }}
-            />
-            <Text
-            style={{
-              position: 'absolute',
-              top: 135,
-              left: 10,
-              fontSize: 18
-            }}> 
-              {data.name} 
-            </Text>
-            <Text
-            style={{
-              position: 'absolute',
-              top: 162.5,
-              left: 15
-            }}> 
-              {data.description} 
-            </Text>
-        </View>
-        <ProfilePage
-        style={{
-          top: -10,
-          left: -8,
-        }}/>
-      </View>
-      );
-    } else {
-      return (
-        <View>
-          <Text>
-            Please View on iOS
-          </Text>
-        </View>
-      )
-    }
-  }
+  return (
+    <View>
+      <ProfilePage/>
+    </View>
+  )
 }
 
 const Search = ({ navigation }) => {
--- components/profilePage.js
+++ components/profilePage.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { Text, View, Platform, Image, FlatList, Dimensions, } from 'react-native';
-import PostOne from '../assets/icon.png'
 
-const windowWidth = Dimensions.get('window').width - 11.5;
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import PostOne from '../assets/icon.png'
+import pfp from "../assets/pfp.png"
+
+const windowWidthLess = Dimensions.get('window').width - 11.5;
 const profileHeight = 355;
 const flatListHeight = Dimensions.get('window').height - profileHeight;
+const windowHeight = Dimensions.get('window').height - 250;
+const windowHeightFull = Dimensions.get('window').height + 5;
+const windowWidth = Dimensions.get('window').width;
 
 const DATA = [
     {
@@ -55,9 +62,89 @@ const DATA = [
   ]
 
 export default function ProfilePage() {
+  const [loadState, setLoadState] = useState(0)
+  const [data, setData] = useState([])
+    
+  if (loadState == 0) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+    if (request.readyState !== 4) {
+        return;
+    }
+    if (request.status === 200) {
+        console.log('success', request.responseText);
+        ResObj = JSON.parse(request.response)
+        setData(ResObj)
+        setLoadState(2)
+    } else {
+        console.warn('error');
+    }
+    };
+    request.open('GET', 'https://userend.herokuapp.com/get_user/636ef181373c83c3a85a2edf');
+    request.send()
+    setLoadState(1)
     return(
-        <View>
-            <FlatList
+      <View>
+        <Text>
+          Loading...
+        </Text>
+      </View>
+    )
+  }
+
+  if ( loadState == 2 ) {
+    if ( Platform.OS === 'ios' ) {
+      return (
+      <View 
+      style={{
+        backgroundColor: 'lightgrey',
+        padding: 8,
+        height: windowHeightFull,
+        position: 'absolute',
+        top: -2
+      }}>
+        <View
+          style={{
+            borderBottomWidth: 3,
+            borderBottomColor: 'black',
+            height: 220,
+            left: -8,
+            top: -10,
+            width: windowWidth,
+            backgroundColor: 'white'
+          }}>
+            <Image
+            source={pfp}
+            style={{
+              height: 100,
+              width: 100,
+              borderRadius: 65,
+              borderColor: 'black',
+              borderWidth: 1,
+              position: 'absolute',
+              top: 35,
+              left: 10
+            }}
+            />
+            <Text
+            style={{
+              position: 'absolute',
+              top: 135,
+              left: 10,
+              fontSize: 18
+            }}> 
+              {data.name} 
+            </Text>
+            <Text
+            style={{
+              position: 'absolute',
+              top: 162.5,
+              left: 15
+            }}> 
+              {data.description} 
+            </Text>
+        </View>
+        <FlatList
             style={{
                 right: 10,
                 height: flatListHeight
@@ -96,6 +183,16 @@ export default function ProfilePage() {
             </View>
             }
             /> 
+      </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>
+            Please View on iOS
+          </Text>
         </View>
-    )
+      )
+    }
+  }
 }
